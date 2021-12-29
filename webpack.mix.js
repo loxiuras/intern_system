297f6369111885b1b90f348563d52162f1150744
loxiuras <petervgarderen@outlook.com>
2021-12-29 14:38:21 +0100

    E - Front template stylesheets via RunDev

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,6 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+mix.postCss('resources/css/argon-dashboard.css', 'public/css', [])
+   .postCss('resources/css/nucleo-icons.css', 'public/css', [])
+   .postCss('resources/css/nucleo-svg.css', 'public/css', [])
