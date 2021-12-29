4db0166f77a7b61b712d47dc2d710a808edf27d7
loxiuras <petervgarderen@outlook.com>
2021-12-29 14:49:31 +0100

    E - User table migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,16 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name', 100);
+            $table->string('insertion', 20);
+            $table->string('last_name', 100);
+            $table->timestamp('date_of_birth')->nullable();
             $table->string('email')->unique();
+            $table->string('telephone');
             $table->timestamp('email_verified_at')->nullable();
+            $table->timestamp('last_login')->nullable();
             $table->string('password');
+            $table->timestamp('last_password_renewal')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
