bd109fe8f3e481823aa35741bde46076099e22ae
PoofDevelopment <37441410+loxiuras@users.noreply.github.com>
2022-01-02 17:04:24 +0100

    A - Hosts database migration

---
+++ database/migrations/2022_01_02_160136_create_hosts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('hosts', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name', 50)->nullable();
+            $table->string('ip-address', 50)->nullable();
+            $table->text('description')->nullable();
+            $table->tinyInteger('active');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('hosts');
+    }
+}
