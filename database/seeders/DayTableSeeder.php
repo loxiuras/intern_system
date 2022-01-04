539ed819622767c0343fe8e203f69cd680d88f74
loxiuras <petervgarderen@outlook.com>
2022-01-04 17:07:56 +0100

    E - Day database seeder

--- database/seeders/DayTableSeeder.php
+++ database/seeders/DayTableSeeder.php
@@ -13,7 +13,7 @@ class DayTableSeeder extends Seeder {
         DB::table('days')->delete();
 
         Day::create([
-            'type'              => 'sick',
+            'type'              => 'free',
             'user_id'           => 1,
             'date'              => '2022-01-01',
             'minutes'           => 60,
@@ -38,6 +38,33 @@ class DayTableSeeder extends Seeder {
             'date'              => '2022-01-03',
             'minutes'           => 180,
             'title'             => 'Test day #3',
+            'description'       => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc molestie pretium nisi tristique tempor. Nullam velit orci, faucibus non sollicitudin et, ultrices eu purus. Vivamus pretium nibh in consequat ultricies.',
+        ]);
+
+        Day::create([
+            'type'              => 'sick',
+            'user_id'           => 1,
+            'date'              => '2022-01-10',
+            'minutes'           => 0,
+            'title'             => 'Sick day #1',
+            'description'       => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc molestie pretium nisi tristique tempor. Nullam velit orci, faucibus non sollicitudin et, ultrices eu purus. Vivamus pretium nibh in consequat ultricies.',
+        ]);
+
+        Day::create([
+            'type'              => 'sick',
+            'user_id'           => 1,
+            'date'              => '2022-01-11',
+            'minutes'           => 0,
+            'title'             => 'Sick day #2',
+            'description'       => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc molestie pretium nisi tristique tempor. Nullam velit orci, faucibus non sollicitudin et, ultrices eu purus. Vivamus pretium nibh in consequat ultricies.',
+        ]);
+
+        Day::create([
+            'type'              => 'sick',
+            'user_id'           => 1,
+            'date'              => '2022-01-12',
+            'minutes'           => 0,
+            'title'             => 'Sick day #3',
             'description'       => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc molestie pretium nisi tristique tempor. Nullam velit orci, faucibus non sollicitudin et, ultrices eu purus. Vivamus pretium nibh in consequat ultricies.',
         ]);
     }
