dbaf110d97077846517fd3dec2beb304fba7b77a
loxiuras <petervgarderen@outlook.com>
2022-01-07 14:32:49 +0100

    A - Style and banner to User ADD

--- resources/views/pages/user/add/index.blade.php
+++ resources/views/pages/user/add/index.blade.php
@@ -1,9 +1,44 @@
 
 @extends('layout.system')
 
+@section('css')
+
+    .pictureDefaultElement {
+        display: none;
+    }
+    .pictureDefaultElement + label {
+        opacity: 0.4;
+        cursor: pointer;
+        position: relative;
+    }
+    .pictureDefaultElement + label .text {
+        display: none;
+        position: absolute;
+        bottom: 20px;
+        right: 0;
+        padding: 2px 10px 2px 15px;
+        color: #ffffff;
+        border-radius: .75rem 0 0 .75rem;
+    }
+    .pictureDefaultElement:checked + label {
+        box-shadow: 0 0px 0 4px #212229;
+        opacity: 1;
+        border-radius: .75rem;
+    }
+    .pictureDefaultElement:checked + label .text {
+        display: block;
+    }
+
+    .bannerBackground {
+        background-size: cover;
+        background-position: center;
+    }
+
+@endsection
+
 @section('pageContent')
 
-    <div class="min-height-300 bg-primary position-absolute w-100 bannerBackground"></div>
+    <div class="min-height-300 bg-primary position-absolute w-100 bannerBackground" style="background-image: url({{ url( "img/banners/banner-". $loginUserData->picture_default_id .".png" ) }})"></div>
 
     @include("layout.sidebar.index")
 
