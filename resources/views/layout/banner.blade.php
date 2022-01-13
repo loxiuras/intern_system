da8848ba6f7e642d5ee0fae9b9b7ec58c9b4112a
WesleyPrijn <74904314+WesleyPrijn@users.noreply.github.com>
2022-01-13 15:24:00 +0100

    E - style of the header

--- resources/views/layout/banner.blade.php
+++ resources/views/layout/banner.blade.php
@@ -1,11 +1,1 @@
-
-@section('css')
-
-    .bannerBackground {
-        background-size: cover;
-        background-position: center;
-    }
-
-@endsection
-
-<div class="min-height-300 bg-primary position-absolute w-100 bannerBackground" style="background-image: url({{ url( "img/banners/banner-". $loginUserData->picture_default_id .".png" ) }})"></div>
+<div class="min-height-300 bg-primary position-absolute w-100 bannerBackground" style="background-size: cover; background-position: center; background-image: url({{ url( "img/banners/banner-". $loginUserData->picture_default_id .".png" ) }})"></div>
