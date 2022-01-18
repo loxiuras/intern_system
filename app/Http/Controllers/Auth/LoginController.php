8ad1a484421513fa93f697d5dc40e3c860a29940
PoofDevelopment <37441410+loxiuras@users.noreply.github.com>
2022-01-18 21:43:56 +0100

    A - Login redirect to dashboard

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -41,6 +41,6 @@ class LoginController extends Controller
         $user->last_login = Carbon::now();
         $user->save();
 
-        // ToDo: Redirect to dashboard overview;
+        return redirect()->route('dashboard');
     }
 }
