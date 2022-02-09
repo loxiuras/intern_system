9f7ee8fe10d913005214a168f3603eb0928206aa
PoofDevelopment <37441410+loxiuras@users.noreply.github.com>
2022-02-09 23:57:20 +0100

    A - Dashboard overview calendar (not finished)

--- resources/views/pages/dashboard/overview/index.blade.php
+++ resources/views/pages/dashboard/overview/index.blade.php
@@ -149,6 +149,25 @@
                 </div>
             </div>
 
+            <div class="row mt-4">
+                <div class="col-12">
+                    <div class="card widget-calendar h-100">
+                        <div class="card-header p-3 pb-0">
+                            <h6 class="mb-0">Calendar</h6>
+                            <div class="d-flex">
+                                <div class="p text-sm font-weight-bold mb-0 widget-calendar-day"></div>
+                                <span>,&nbsp;</span>
+                                <div class="p text-sm font-weight-bold mb-1 widget-calendar-year d-none"></div>
+                                <div class="p text-sm font-weight-bold mb-0 widget-calendar-month"></div>
+                            </div>
+                        </div>
+                        <div class="card-body p-3">
+                            <div data-toggle="widget-calendar"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </div>
     </main>
 
@@ -157,5 +176,81 @@
 @section('js')
 
     <script src="{{ url('js/sidebar.js') }}"></script>
+
+    <script src="{{ url('js/plugins/fullcalendar.js') }}"></script>
+    <script>
+
+        if (document.querySelector('[data-toggle="widget-calendar"]')) {
+            let calendarEl = document.querySelector('[data-toggle="widget-calendar"]');
+            let today = new Date();
+
+            
+        }
+
+        /*
+        if (document.querySelector('[data-toggle="widget-calendar"]')) {
+            var calendarEl = document.querySelector('[data-toggle="widget-calendar"]');
+            var today = new Date();
+            var mYear = today.getFullYear();
+            var weekday = ["Zondag", "Maandag", "Dinsdag", "Woensdag", "Donderdag", "Vrijdag", "Zaterdag"];
+            var months = ["januari", "febuari", "maart", "april", "mei", "juni", "juli", "ausgustus", "september", "oktober", "november", "december"];
+            var mDay = weekday[today.getDay()];
+            var mMonth = months[today.getMonth()];
+
+            var m = today.getMonth();
+            var d = today.getDate();
+            document.getElementsByClassName('widget-calendar-year')[0].innerHTML = mYear;
+            document.getElementsByClassName('widget-calendar-month')[0].innerHTML = mMonth;
+            document.getElementsByClassName('widget-calendar-day')[0].innerHTML = mDay;
+
+            var calendar = new FullCalendar.Calendar(calendarEl, {
+                contentHeight: 'auto',
+                initialView: 'dayGridMonth',
+                selectable: true,
+                initialDate: '2020-12-01',
+                editable: true,
+                weekNumbers: true,
+                headerToolbar: false,
+                    events: [{
+                        title: 'Carel: Call with Dave',
+                        start: '2020-12-03',
+                        end: '2020-12-03',
+                        className: 'bg-gradient-info'
+                    },
+
+                    {
+                        title: 'Peter: Lunch meeting',
+                        start: '2020-12-01',
+                        end: '2020-12-01',
+                        className: 'bg-gradient-danger'
+                    },
+
+                    {
+                        title: 'Wesley: All day conference',
+                        start: '2020-12-05',
+                        end: '2020-12-05',
+                        className: 'bg-gradient-dark'
+                    },
+
+                    {
+                        title: 'Peter: Winter Hackaton',
+                        start: '2020-12-03',
+                        end: '2020-12-03',
+                        className: 'bg-gradient-danger'
+                    },
+
+                    {
+                        title: 'Carel: Digital event',
+                        start: '2020-12-07',
+                        end: '2020-12-09',
+                        className: 'bg-gradient-info'
+                    },
+
+
+                ]
+            });
+            calendar.render();
+        }*/
+    </script>
 
 @endsection
