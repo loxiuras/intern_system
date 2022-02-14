ff190deabf7ff0c7f80a06345a37d83f38fcc7c3
PoofDevelopment <37441410+loxiuras@users.noreply.github.com>
2022-02-15 00:06:29 +0100

    A - TicketUser migration

---
+++ database/migrations/2022_02_14_230025_create_ticket_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTicketUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ticket_users', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger("ticket_id")->unsigned();
+            $table->foreign('ticket_id')->references('id')->on('tickets')->onUpdate('cascade')->onDelete('cascade');
+            $table->bigInteger("user_id")->unsigned();
+            $table->foreign('user_id')->references('id')->on('users')->onUpdate('cascade')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ticket_users');
+    }
+}
