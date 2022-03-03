d54855b0637d81b46fbcca29fc9f3de4d002b2dc
PoofDevelopment <37441410+loxiuras@users.noreply.github.com>
2022-03-03 20:48:41 +0100

    E - Ticket seeder

--- database/factories/TicketFactory.php
+++ database/factories/TicketFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Carbon;
 
 class TicketFactory extends Factory
 {
@@ -13,7 +14,8 @@ class TicketFactory extends Factory
      */
     public function definition()
     {
-        $scheduledDate = $this->faker->date();
+        $date = Carbon::now()->addDays( rand( 0, 10) );
+        $startDate = $date->subDays( rand( 0, 20 ) );
 
         return [
             'company_id'          => 1,
@@ -26,8 +28,8 @@ class TicketFactory extends Factory
             'invoice_description' => $this->faker->text( 150 ),
             'invoice_price'       => $this->faker->numberBetween( 1000, 10000 ),
             'invoice_time'        => $this->faker->numberBetween( 60, 600 ),
-            'scheduled_date'      => $scheduledDate,
-            'scheduled_end_date'  => $scheduledDate,
+            'scheduled_date'      => $startDate,
+            'scheduled_end_date'  => $startDate->addDays( rand( 1, 3 ) ),
             'status'              => $this->faker->numberBetween( 1, 4 ),
         ];
     }
